8469fca11ca3ac00923e38593f82647b8ac92995
Beny Hasanudin <bhasanudin@gmail.com>
2024-10-24 08:53:40 +0700

    By default all variables are immutable,
    Once it is declared cannot be reassigned.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 fn main() {
     let x: i32; // declaration without initialization
     x = 5; // Initialization
+    x = 20; // Error: cannot assign twice of immutable variable
     print!("{}",x)
 }
